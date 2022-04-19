02894f0deec1cc6c14d1269baef1f455337b17e7
gaBUBUNts <89221310+gaBUBUNts@users.noreply.github.com>
2022-04-20 01:07:50 +0300

    Add files via upload

---
+++ src/circular_buffer.cpp
@@ -0,0 +1,49 @@
+#include "circular_buffer.hpp"
+#include <stdexcept>
+
+namespace itis {
+  CircularBuffer::CircularBuffer(int capacity) {
+    if (capacity <= 0) {
+      throw std::invalid_argument("capacity is not positive");
+    }
+    capacity_ = capacity;
+  }
+
+  void CircularBuffer::Enqueue(int value) {
+    if (size_ < capacity_) {
+      LinkedQueue::Enqueue(value);
+      back_->next = front_;
+    }
+    else {
+        Dequeue();
+        LinkedQueue::Enqueue(value);
+        back_->next = front_;
+    }
+  }
+
+  bool CircularBuffer::Dequeue() {
+    if (size_ == 0) {
+      return false;
+    }
+    if (size_ == 1) {
+      delete front_;
+      delete back_;
+      size_ -= 1;
+      return true;
+    }
+    size_ -= 1;
+    Node* temp = front_;
+    front_ = front_->next;
+    delete temp;
+    return true;
+  }
+
+  bool CircularBuffer::Resize(int new_capacity) {
+    if (new_capacity > capacity_) {
+      capacity_ = new_capacity;
+      return true;
+    }
+    return false;
+  }
+
+} // namespace itis
---
+++ src/linked_queue.cpp
@@ -0,0 +1,108 @@
+#include "linked_queue.hpp"
+
+namespace itis {
+
+  LinkedQueue::~LinkedQueue() {
+
+    // эквивалентно очистке очереди
+    LinkedQueue::Clear();
+  }
+
+  void LinkedQueue::Enqueue(int value) {
+    Node* node = new Node(value);
+    if (front_ == nullptr) {
+      front_ = node;
+      back_ = front_;
+    }
+    else {
+      back_->next = node;
+      back_ = node;
+    }
+    size_ += 1;
+  }
+
+  bool LinkedQueue::Dequeue() {
+    if (size_ != 0) {
+      front_ = front_->next;
+      size_ -= 1;
+      return true;
+    }
+    return false;
+  }
+
+  void LinkedQueue::Clear() {
+      Node* node_to_remove = front_;
+      for (int i = 0; i < size_-1; i++) {
+        auto temp = node_to_remove;
+        node_to_remove = node_to_remove->next;
+        delete temp;
+      }
+      delete node_to_remove;
+      front_ = nullptr;
+      back_ = nullptr;
+      size_ = 0;
+  }
+
+  std::optional<int> LinkedQueue::front() const {
+    if (front_ != nullptr) {
+      return front_->value;
+    }
+    return std::nullopt;
+  }
+
+  std::optional<int> LinkedQueue::back() const {
+    if (back_ != nullptr) {
+      return back_->value;
+    }
+    return std::nullopt;
+  }
+
+  bool LinkedQueue::IsEmpty() const {
+    return front_ == nullptr;
+  }
+
+  int LinkedQueue::size() const {
+    return size_;
+  }
+
+  // ДЛЯ ТЕСТИРОВАНИЯ
+  LinkedQueue::LinkedQueue(const std::vector<int>& values) {
+
+    if (values.empty()) {
+      return;
+    }
+
+    auto* curr_node = new Node(values.front());
+    front_ = curr_node;
+
+    for (int index = 1; index < values.size() - 1; ++index) {
+      curr_node->next = new Node(values[index]);
+      curr_node = curr_node->next;
+    }
+
+    if (values.size() == 1) {
+      back_ = front_;
+    } else {
+      curr_node->next = new Node(values.back());
+      back_ = curr_node->next;
+    }
+
+    size_ = static_cast<int>(values.size());
+  }
+
+  std::vector<int> LinkedQueue::toVector() const {
+    if (front_ == nullptr || size_ == 0) {
+      return {};
+    }
+
+    std::vector<int> array;
+    array.reserve(size_);
+
+    for (auto* node = front_; node != nullptr; node = node->next) {
+      array.push_back(node->value);
+    }
+
+    return array;
+  }
+
+}  // namespace itis
